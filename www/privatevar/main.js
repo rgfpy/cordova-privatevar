beee4d25e518c28aaf4c5b59cfaafa8116a59aed
NB Acer <r2galeano@gmail.com>
2017-07-06 16:08:17 -0400

    fix xml

--- www/privatevar/main.js
+++ www/privatevar/main.js
@@ -1,6 +1,6 @@
 /*global cordova, module*/
 module.exports = {
     get: function (name, successCallback, errorCallback) {
-		cordova.exec(successCallback, errorCallback, "customprivatevar", "get", [name]);
+		cordova.exec(successCallback, errorCallback, "privatevar", "get", [name]);
     }
 };
