92ddfa820d9418778927c8e427d6e941c19f50d7
NB Acer <r2galeano@gmail.com>
2017-07-07 10:33:53 -0400

    Simplify code

--- www/privatevar.js
+++ www/privatevar.js
@@ -1,11 +1,5 @@
-cordova.define("com-sistepar-cordova-privatevar.PrivatevarPlugin", function(require, exports, module) {
-var exec = require('cordova/exec');
-var platform = require('cordova/platform');
-	
 module.exports = {
 	get: function (parmPost, successCallback, errorCallback) {
 		exec(successCallback, errorCallback, "PrivatevarPlugin", "get", [parmPost]);
 	}
 };
-
-});
